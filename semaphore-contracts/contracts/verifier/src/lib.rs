e22be15ed7159cde41b331b3c093df3b71e3fa61
MPSxDev <mparedes.digital@gmail.com>
2024-11-04 01:07:04 -0300

    feat: add structs

--- semaphore-contracts/contracts/verifier/src/lib.rs
+++ semaphore-contracts/contracts/verifier/src/lib.rs
@@ -1,2 +1,19 @@
 #![no_std]
 use soroban_sdk::{contract, contractimpl, storage::Storage, Env, Vec};
+
+#[derive(Clone)]
+pub struct VerifyingKey(Vec<u8>);
+
+#[derive(Clone)]
+pub struct SemaphoreProof {
+    proof_data: Vec<u8>,
+    public_inputs: Vec<u8>,
+}
+
+#[derive(Clone, Debug)]
+#[repr(u32)]
+pub enum Error {
+    InvalidProof = 1,
+    InvalidVerificationKey = 2,
+    VerificationFailed = 3,
+}
