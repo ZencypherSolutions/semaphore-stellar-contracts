111f1e215a379f837aebea95857ba95b01abd43b
MPSxDev <mparedes.digital@gmail.com>
2024-11-05 22:03:43 -0300

    feat(verifier): Import used dependencies

---
+++ semaphore-contracts/contracts/verifier/src/lib.rs
@@ -0,0 +1,8 @@
+use ark_bn254::{Bn254, Fr, G1Affine, G2Affine};
+use ark_ec::{AffineCurve, PairingEngine, ProjectiveCurve};
+use ark_ff::{PrimeField, Fp256};
+use ark_std::{One, Zero};
+use std::str::FromStr;
+use ark_ff::QuadExtField;
+use ark_bn254::Fq2Parameters;
+use ark_ff::Fp2ParamsWrapper;
